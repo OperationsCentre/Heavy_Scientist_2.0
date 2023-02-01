9a8d27cd42a55521e90d77563230e4a6db315e71
joshjh2002 <joshhelman@hotmail.co.uk>
2023-02-01 23:13:12 +0000

    Rearranged interaction if blocks

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,22 +3,6 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
-    if (interaction.isButton()) {
-      const button = interaction.client.buttons.get(interaction.customId);
-
-      if (!button) {
-        console.error(`No button matching ${interaction.customId} was found.`);
-        return;
-      }
-
-      try {
-        await button.execute(interaction);
-      } catch (error) {
-        console.error(`Error executing ${interaction.customId}`);
-        console.error(error);
-      }
-    }
-
     if (interaction.isCommand()) {
       const command = interaction.client.commands.get(interaction.commandName);
 
@@ -33,6 +17,20 @@ module.exports = {
         await command.execute(interaction);
       } catch (error) {
         console.error(`Error executing ${interaction.commandName}`);
+        console.error(error);
+      }
+    } else if (interaction.isButton()) {
+      const button = interaction.client.buttons.get(interaction.customId);
+
+      if (!button) {
+        console.error(`No button matching ${interaction.customId} was found.`);
+        return;
+      }
+
+      try {
+        await button.execute(interaction);
+      } catch (error) {
+        console.error(`Error executing ${interaction.customId}`);
         console.error(error);
       }
     }
