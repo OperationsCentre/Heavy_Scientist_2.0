7c70eb92c3478235cba29c02f0bd968bab75832a
joshjh2002 <joshhelman@hotmail.co.uk>
2023-05-09 22:21:16 +0100

    Modal to input suggestion

--- buttons/make-suggestion-button.js
+++ buttons/make-suggestion-button.js
@@ -1,9 +1,27 @@
+const {
+  ModalBuilder,
+  TextInputBuilder,
+  ActionRowBuilder,
+  TextInputStyle,
+} = require("discord.js");
+
 module.exports = {
   data: { name: "make-suggestion-button" },
   async execute(interaction) {
-    await interaction.reply({
-      content: "Thank you for your suggestion",
-      ephemeral: true,
-    });
+    const supportModal = new ModalBuilder()
+      .setCustomId("suggestion-modal")
+      .setTitle("Make a Suggestion");
+
+    const issue = new TextInputBuilder()
+      .setCustomId("suggestion")
+      .setLabel("What's your suggestion")
+      // Paragraph means multiple lines of text.
+      .setStyle(TextInputStyle.Paragraph);
+
+    const actionRow = new ActionRowBuilder().addComponents(issue);
+
+    supportModal.addComponents(actionRow);
+
+    await interaction.showModal(supportModal);
   },
 };
