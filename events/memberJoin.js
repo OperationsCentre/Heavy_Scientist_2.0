f38ffd90b550218e7d5f1c56a5406873b0dff6e5
joshjh2002 <joshhelman@hotmail.co.uk>
2023-06-30 19:52:08 +0100

    Removed join message

--- events/memberJoin.js
+++ events/memberJoin.js
@@ -11,11 +11,15 @@ module.exports = {
   // when a member joins the server, send a message to the greeter channel and log it.
   async execute(member) {
     // Create a picture of the member.
+
+    /*
     const memberPicture = await createJoinPicture(member);
     greeter.send({
       content: `${member.user.toString()}`,
       files: [memberPicture],
     });
+    */
+
     logger.send({ embeds: [member_join(member)] });
     debug.log(`${member.user.tag} has now joined the server!`);
   },
