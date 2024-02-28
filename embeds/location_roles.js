4bae7e28d0fc18093a91a3504180edd5627ed926
joshjh2002 <joshhelman@hotmail.co.uk>
2024-02-28 17:09:02 +0000

    Suggestions and roles

--- embeds/location_roles.js
+++ embeds/location_roles.js
@@ -1,13 +1,11 @@
 const { colours } = require("../config/config.json");
-const { na_role, eu_role, row_role } = require("../config/config.json").roles;
+const { na_role, eu_role, africa_role, oceana_role, south_america_role } =
+  require("../config/config.json").roles;
 const { rust_logo } = require("../config/config.json").images;
 module.exports = {
   location_roles: {
     title: "Locations Roles",
     color: colours.rust_colour,
-    thumbnail: {
-      url: rust_logo,
-    },
     fields: [
       {
         name: ":one:",
@@ -21,7 +19,17 @@ module.exports = {
       },
       {
         name: ":three:",
-        value: `<@&${row_role}>`,
+        value: `<@&${africa_role}>`,
+        inline: true,
+      },
+      {
+        name: ":four:",
+        value: `<@&${oceana_role}>`,
+        inline: true,
+      },
+      {
+        name: ":five:",
+        value: `<@&${south_america_role}>`,
         inline: true,
       },
     ],
